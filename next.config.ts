465efde0f83c95afabef3cba37e34373ac25ce59
Himanshu Sandal <sandal.himanshu98@gmail.com>
2026-09-07 13:50:27 +0530

    Configure website for local development

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/trinetra-arc",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
